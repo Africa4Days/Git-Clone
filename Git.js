bedb49c32227e52d1a8a7d9067a50d9f63090470
Christopher Bell <christopherbell@Christophers-MacBook-Pro.local>
2019-02-05 16:31:36 -0500

    Add the log method to the git class

--- Git.js
+++ Git.js
@@ -1,29 +1,39 @@
+class Commit {
+  constructor(id, parent, message) {
+    this.id = id; // commit id
+    this.message = message; // commit message
+    this.parent = parent; // the commits parent
+  }
+}
+
 class Git {
   constructor(name) {
-    this.name = name;
-    this.lastCommitId = -1;
-    this.HEAD = null;
+    this.name = name; // repo name
+    this.lastCommitId = -1; // last commit id
+    this.HEAD = null; // reference to the last commit
   }
 
   commit(message) {
-    let commit = new Commit(++this.lastCommitId, message);
+    let commit = new Commit(++this.lastCommitId, this.HEAD, message);
+    this.HEAD = commit; // keeping track of the commits with the HEAD prop
     return commit;
   }
 
   log() {
+    let commit = this.HEAD;
     let history = [];
 
+    while (commit) {
+      history.push(commit);
+      commit = commit.parent;
+    }
+
+    console.log(history);
     return history;
   }
 }
 
-class Commit {
-  constructor(id, message) {
-    this.id = id;
-    this.message = message;
-  }
-}
-
-let repo = new Git("simple-repo");
+let repo = new Git("repo-sample");
 repo.commit("Initial commit");
-console.log(repo);
+repo.commit("This is the second commit");
+repo.log();
